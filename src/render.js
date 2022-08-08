055fdd1ed00b41d46154c26829cb979d99d23377
Byeraon <nikitka.crasnov2013@mail.ru>
2022-08-08 17:43:40 +0300

    fix: change fuckink Akim rerender

--- src/render.js
+++ src/render.js
@@ -1,15 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { addPost } from "./components/redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 export let rerenderEntireTree = (state) => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <BrowserRouter>
-      <App state={state} addPost={addPost()} />
-    </BrowserRouter>
-  );
+  let container = null;
+
+  if (!container) {
+    container = document.getElementById("root");
+    const root = createRoot(container);
+
+    root.render(
+      <BrowserRouter>
+        <App state={state} addPost={addPost} />
+      </BrowserRouter>
+    );
+  }
 };
